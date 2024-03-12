e953350d6e6a73861d11712f6dee928d8be50e63
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-03-13 00:39:32 +0900

    feat: add init project files

--- board/build.gradle.kts
+++ board/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter")
+
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
