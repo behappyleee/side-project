c490c90a763a73a5abdf5a4a24d439907f106c69
behappyleee <dhfhfkwhdk@naver.com>
2024-07-15 23:55:54 +0900

    studying commit

---
+++ open-api-data/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "com.canbe"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // HttpInterface 사용하기 위하여 추가
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ open-api-data/src/test/kotlin/com/canbe/openapidata/OpenApiDataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.canbe.openapidata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenApiDataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
