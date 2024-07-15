fd8cd5b5a58b415ce05cf69631c5efaeab320d6e
behappyleee <dhfhfkwhdk@naver.com>
2024-07-16 00:21:05 +0900

    studying commit

--- open-api-data/build.gradle.kts
+++ open-api-data/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("org.springframework.boot") version "3.3.1"
-    id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.canbe"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "side-project"
+
+include("open-api-data")
